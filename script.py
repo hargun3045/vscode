e8557971224296c53a73c96d4e13fc3ba8322db6
hargun3045 <hargun3045@gmail.com>
2021-07-13 15:41:58 +0530

    Removed some code

--- script.py
+++ script.py
@@ -1,19 +1,5 @@
-import math
-import sys
-from os import rename
-
 import requests
 
-print(sys.version)
-print(sys.executable)
 
-
-def greet():
-    # name = "Hargun"
-    name = input("What is your name?\n")
-    print(f"Hello {name}")
-
-
-greet()
 r = requests.get("https://coreyms.com")
 print(r.status_code)
